ef1c23041e3973f37f2a4499707d347d8c134ab0
Martin Lorenz <martin.lorenz@atos.net>
2023-11-02 15:32:47 +0100

    aws-lambda-layer: update to reventless-aws@1.3.14

--- packages/aws-lambda-layer/example/index.js
+++ packages/aws-lambda-layer/example/index.js
@@ -11,7 +11,7 @@ const dependenciesPath = resolvePath(pathToLayerData, pathToSavedDependencies);
 const opt = {
     //pathToSourcePackage: '../reventless-aws',
     sourcePackageName: '@reventless/reventless-aws',
-    sourcePackageVersion: '1.3.13',
+    sourcePackageVersion: '1.3.14',
     pathToLayerData,
     pathToSavedDependencies,
     excludeScopes: ['pulumi', 'types', 'opentelemetry'],
