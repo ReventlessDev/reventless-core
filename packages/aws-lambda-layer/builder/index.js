bee501ebd4ed225dfa4dcddbb93a61f813cc9ef6
Martin Lorenz <martin.lorenz@atos.net>
2024-12-09 10:15:34 +0100

    aws-lambda-layer: update to reventless-aws@2.0.0-rescript11.26

--- packages/aws-lambda-layer/builder/index.js
+++ packages/aws-lambda-layer/builder/index.js
@@ -11,7 +11,7 @@ const dependenciesPath = resolvePath(pathToLayerData, pathToSavedDependencies);
 const opt = {
     //pathToSourcePackage: '../reventless-aws',
     sourcePackageName: '@reventless/reventless-aws',
-    sourcePackageVersion: '2.0.0-rescript11.25',
+    sourcePackageVersion: '2.0.0-rescript11.26',
     pathToLayerData,
     pathToSavedDependencies,
     excludeScopes: ['pulumi', 'types', 'opentelemetry'],
