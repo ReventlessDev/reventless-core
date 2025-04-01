82873d2fcfe836e7e1855ae5da254ffef2532339
Martin Lorenz <martin.lorenz@atos.net>
2025-04-01 13:36:21 +0200

    aws-lambda-layer: update to reventless-aws@2.0.3-policies.62

--- packages/aws-lambda-layer/builder/builder.js
+++ packages/aws-lambda-layer/builder/builder.js
@@ -32,7 +32,7 @@ const npmPackages = ['npm-bundled', 'npm-install-checks', 'npm-normalize-package
 //IncludePrecompiledModules are ones that are required for applications and therefore are not in reventless. 
 const options = {
   sourcePackageName: '@reventless/reventless-aws',
-  sourcePackageVersion: '2.0.3-policies.61',
+  sourcePackageVersion: '2.0.3-policies.62',
   pathToLayerData,
   pathToSavedDependencies,
   pathToPrecompiled,
